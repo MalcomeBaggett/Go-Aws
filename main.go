9071f126de1e13f48387474ad2ebc9e914169790
Malcome Baggett <MalcomeBaggett@gmail.com>
2024-09-25 23:31:03 -0400

    added imports

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/malcomebaggett/Go-Aws/imports"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	newTicket := imports.Ticket{
+		ID:    123,
+		Event: "Yoooo",
+	}
+	fmt.Println(newTicket)
+	newTicket.PrintEvent()
 }
--- structs/main.go
+++ structs/main.go
@@ -22,9 +22,19 @@ func main() {
 	}
 	a := 7
 	b := &a
+	*b = 9
+	// The above code will derefernce to the original variable of a and turn it to 9 because b was pointing to the same memory adress as a
 	fmt.Println(b)
 	myPerson.changeName("mr x")
 	anotherNewPerson := NewPerson("M", 100)
 	fmt.Println(myPerson.Name)
 	fmt.Println(anotherNewPerson)
+
+	mySlicor := []int{
+		1, 2, 3,
+	}
+
+	for _, value := range mySlicor {
+		fmt.Println((value))
+	}
 }
